66e1cb64e1a1d706214ff4b243bf373be33af4c9
Gabriel Abreu <Gabriel.Abreu@mindbodyonline.com>
2025-11-29 11:06:57 +0000

    Including scheduler job

---
+++ stock_trading_python_app/scheduler.py
@@ -0,0 +1,21 @@
+import schedule
+import time
+from script import run_stock_job
+
+from datetime import datetime
+
+def basic_job():
+    print("Job starte at:", datetime.now())
+
+# Run every minute
+schedule.every().minute.do(basic_job)
+
+# Run every minute
+schedule.every().day.at("09:00").do(run_stock_job)
+
+# Run every minute
+schedule.every().minute.do(basic_job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
